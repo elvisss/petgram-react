c1a9a5193ecf7efe61a7887e363c572f7efac621
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2020-08-03 21:24:40 -0500

    adding protected pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+/* eslint-disable no-confusing-arrow */
 import React from 'react';
 import { Router } from '@reach/router';
 import Layout from './layout';
 import Home from './pages/Home';
+import User from './pages/User';
+import Favs from './pages/Favs';
+import NotRegisteredUser from './pages/NotRegisteredUser';
 import Detail from './pages/Detail';
+
+const UserLogged = ({ children }) => children({ isAuth: false });
 
 const App = () => (
   <Layout>
@@ -11,6 +17,22 @@ const App = () => (
       <Home path="/pet/:categoryId" />
       <Detail path="/detail/:detailId" />
     </Router>
+    <UserLogged>
+      {
+        ({ isAuth }) => isAuth
+          ? (
+            <Router>
+              <Favs path="/favs" />
+              <User path="/user" />
+            </Router>
+          ) : (
+            <Router>
+              <NotRegisteredUser path="/favs" />
+              <NotRegisteredUser path="/user" />
+            </Router>
+          )
+      }
+    </UserLogged>
   </Layout>
 );
 
---
+++ src/pages/NotRegisteredUser.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NotRegisteredUser = () => <h1>NotRegisteredUser</h1>;
+
+export default NotRegisteredUser;
