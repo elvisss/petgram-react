137445a7218d12ba2e54287ed3582342b04ecbb8
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2020-08-09 02:01:49 -0500

    adding 404 page

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
-/* eslint-disable no-confusing-arrow */
-import React from 'react';
-import { Router } from '@reach/router';
+import React, { useContext } from 'react';
+import { Router, Redirect } from '@reach/router';
+
 import Layout from './layout';
 import Home from './pages/Home';
 import User from './pages/User';
 import Favs from './pages/Favs';
-import NotRegisteredUser from './pages/NotRegisteredUser';
 import Detail from './pages/Detail';
-import Context from './Context';
+import NotFound from './pages/NotFound';
+import NotRegisteredUser from './pages/NotRegisteredUser';
 
-const App = () => (
-  <Layout>
-    <Router>
-      <Home path="/" />
-      <Home path="/pet/:categoryId" />
-      <Detail path="/detail/:detailId" />
-    </Router>
-    <Context.Consumer>
-      {
-        ({ isAuth }) => isAuth
-          ? (
-            <Router>
-              <Favs path="/favs" />
-              <User path="/user" />
-            </Router>
-          ) : (
-            <Router>
-              <NotRegisteredUser path="/favs" />
-              <NotRegisteredUser path="/user" />
-            </Router>
-          )
-      }
-    </Context.Consumer>
-  </Layout>
-);
+import { Context } from './Context';
+
+const App = () => {
+  const { isAuth } = useContext(Context);
+  return (
+    <Layout>
+      <Router>
+        <NotFound default />
+        <Home path="/" />
+        <Home path="/pet/:categoryId" />
+        <Detail path="/detail/:detailId" />
+        {!isAuth && <NotRegisteredUser path="/login" />}
+        {!isAuth && <Redirect noThrow from="/favs" to="/login" />}
+        {!isAuth && <Redirect noThrow from="/user" to="/login" />}
+        {isAuth && <Redirect noThrow from="/login" to="/" />}
+        <Favs path="/favs" />
+        <User path="/user" />
+      </Router>
+    </Layout>
+  );
+};
 
 export default App;
